f7b794c34087bb34a83a4a82516bf7f7113524c3
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-08-01 23:47:11 +0700

    Lab 6 | Learn For Each , For of, for in

---
+++ Lab6/js/lab6-1.js
@@ -0,0 +1,28 @@
+let menus = document.querySelectorAll(".title");
+let openText = document.querySelectorAll(".myText");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// dạng 1 chỉ cần value
+numbers.forEach((num) => {
+  console.log(num);
+});
+
+// gán value
+for (const num1 of numbers) {
+  console.log(num1);
+}
+
+// Đếm số index
+for (const num2 in numbers) {
+  console.log(num2);
+}
+//Đề: Cho mảng sau, hãy dùng forEach() để tính tổng các số chẵn trong mảng.
+const numbersTong = [3, 6, 1, 8, 4, 5, 10];
+
+numbersTong.forEach((numTon) => {
+  if (numTon % 2 == 0) {
+    let total = +numTon;
+  }
+  console.log(total);
+});
+
