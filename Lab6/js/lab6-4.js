5b4a6ce242b6163fec418683b71668bfbd3e6370
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-08-04 22:09:08 +0700

    Lab 6 - Cau 4

--- Lab6/js/lab6-4.js
+++ Lab6/js/lab6-4.js
@@ -1,3 +1,43 @@
 const output = document.querySelector(".output");
 const message = document.querySelector(".message");
+const startBtn = document.getElementById("startBtn");
 
+const box = document.createElement("div");
+box.classList.add("box");
+output.appendChild(box);
+
+const game = {
+  timer: null,
+  start: null,
+};
+
+function ranNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function addBox() {
+  message.textContent = "Click nhanh lên!";
+  game.start = new Date().getTime();
+
+  box.style.left = ranNum(450) + "px";
+  box.style.top = ranNum(450) + "px";
+  box.style.display = "block";
+}
+
+box.addEventListener("click", () => {
+  if (!game.start) return; // chưa bắt đầu mà click thì bỏ qua
+
+  box.style.display = "none";
+  const currentTime = new Date().getTime();
+  const duration = ((currentTime - game.start) / 1000).toFixed(2);
+  message.textContent = `Bạn mất ${duration} giây để phản xạ.`;
+
+  game.start = null;
+  game.timer = setTimeout(addBox, ranNum(3000) + 1000);
+});
+
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  message.textContent = "Đợi chút, chuẩn bị...";
+  setTimeout(addBox, ranNum(2000) + 1000);
+});
