f005315350b8480528ec79b00ae643e286831d69
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-10-18 20:19:46 +0700

    Nested Loop

---
+++ F8/loop/nested-loop/nested-loop.js
@@ -0,0 +1,14 @@
+// Video 103: Vòng lặp lòng nhau - Nested Loop
+console.log("Video 103: Vòng lặp lòng nhau - Nested Loop");
+
+let myArray = [
+    [2,3],
+    [4,5],
+    [6,7],
+]
+
+for (let i = 0; i < myArray.length; i++) {
+    for (let j = 0; j < myArray[i].length; j++) {
+      console.log(myArray[i][j]);
+    }
+}
