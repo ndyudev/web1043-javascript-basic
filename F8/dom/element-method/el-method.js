ac45daa0b18be39d2310810d86bd8b95180a0c14
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-10-27 20:47:21 +0700

    [DOM]: by tag name, class name, query , all, node list, collection

--- F8/dom/element-method/el-method.js
+++ F8/dom/element-method/el-method.js
@@ -4,4 +4,20 @@ let container = document.getElementsByClassName("container")
 
 let heading = document.getElementById("heading");
 
-console.log(heading);
+let body = document.getElementsByClassName("body")
+
+let tag = document.getElementsByTagName("p")
+
+let heading2 = document.querySelector(".hd2");
+
+let heading2All = document.querySelectorAll(".hd2");
+
+console.log(heading2All[1]); // node list như array
+
+
+// HTML Collection
+
+console.log({
+  element: heading,
+  body: body
+});
